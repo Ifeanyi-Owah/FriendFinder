339d86f478225eb1b174e76858d788d3b94fafd3
Ifeanyi-Owah <ifeanyi_owah@yahoo.com>
2019-02-08 18:55:52 -0500

    friends updated

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+  {
+    name: "Will Smith",
+    photo: "https://timedotcom.files.wordpress.com/2015/02/will-smith1.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Tom Cruise",
+    photo: "http://www.gstatic.com/tv/thumb/persons/378/378_v9_bd.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Brad Pitt",
+    photo: "http://www.gstatic.com/tv/thumb/persons/1366/1366_v9_bb.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Al Pacino",
+    photo: "http://www.gstatic.com/tv/thumb/persons/2657/2657_v9_ba.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Emma Watson",
+    photo: "http://www.gstatic.com/tv/thumb/persons/247026/247026_v9_bb.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Anne Hathaway",
+    photo: "http://www.gstatic.com/tv/thumb/persons/205765/205765_v9_bb.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+module.exports = friends;
