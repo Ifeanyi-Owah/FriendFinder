d401d3bc8a1fc6746c2f103f7a23369dfded156a
Ifeanyi-Owah <ifeanyi_owah@yahoo.com>
2019-02-23 18:42:47 -0500

    friendfinder functional commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,38 @@
-
+const express = require("express");
+const router = express.Router();
 
 
 var friends = require("../data/friends");
+
+router.get("/friends", function (req, res) {
+   res.json(friends);
+});
+
+router.post("/friends", function (req, res) {
+   var bestMatch = {
+      name: "",
+      photo: "",
+      diff: 1000000
+   };
+   var newUser = req.body;
+
+   friends.forEach(function (friend) {
+      var userScores = newUser.scores;
+      var matchScores = friend.scores;
+      var diff = 0;
+      for (var i = 0; i < 10; i++) {
+         diff = diff + (Math.abs(parseInt(userScores[i]) - parseInt(matchScores[i])));
+      }
+      if (diff < bestMatch.diff) {
+         bestMatch.name = friend.name;
+         bestMatch.photo = friend.photo;
+         bestMatch.diff = diff;
+      }
+   });
+
+   friends.push(newUser);
+   // console.log(friends);
+   res.json(bestMatch);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const htmRoutes = require('./app/routing/htmlRoutes');
+const apiRoutes = require('./app/routing/apiRoutes');
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(apiRoutes);
 app.use(htmRoutes);
    
 
