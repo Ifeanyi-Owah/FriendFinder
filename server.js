72d6d252a4bc03960ab11c373d4f8d23bc2d27a0
Ifeanyi-Owah <ifeanyi_owah@yahoo.com>
2019-02-23 18:59:15 -0500

    updated server port

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express();
 const htmRoutes = require('./app/routing/htmlRoutes');
 const apiRoutes = require('./app/routing/apiRoutes');
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+var PORT = process.env.PORT || 8080;
 
 app.use(apiRoutes);
 app.use(htmRoutes);
    
-
-  
-
-app.listen(3000);
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
